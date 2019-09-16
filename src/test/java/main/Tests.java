18420e4f0d3f00684f15d1f5834ee6c751d8be1a
yuriybocharov <bocharov0682@gmail.com>
2019-09-16 20:50:04 +0300

    test tv.tenet.ua

--- src/test/java/main/Tests.java
+++ src/test/java/main/Tests.java
@@ -2,15 +2,10 @@ package main;
 
 
 
-import javafx.scene.shape.MoveTo;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Locatable;
-import org.openqa.selenium.interactions.MoveTargetOutOfBoundsException;
+
 import org.openqa.selenium.interactions.internal.Coordinates;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -20,7 +15,6 @@ import pages.*;
 import tools.*;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -43,17 +37,22 @@ public class Tests {
         element.click ();
         WebElement element1 = driver.findElement (By.cssSelector (TenetPage.login));
         element1.sendKeys (Variables.login);
-        Thread.sleep (1000);
+
         WebElement element2 = driver.findElement (By.cssSelector (TenetPage.password));
         element2.sendKeys (Variables.password);
-        Thread.sleep (1000);
+
         WebElement element3 = driver.findElement (By.cssSelector (TenetPage.next));
         element3.click ();
-        Thread.sleep (2000);
-        WebElement element4 = driver.findElement (By.cssSelector (TenetPage.channel53));
-        scrollToElement (element4);
-        System.out.println (driver.findElement (By.cssSelector (TenetPage.channel53)).getText ());
-        driver.findElement (By.cssSelector (TenetPage.channel53)).click ();
+
+//        WebElement element4 = driver.findElement (By.cssSelector (TenetPage.channel53));
+//        scrollToElement (element4);
+//        System.out.println (driver.findElement (By.cssSelector (TenetPage.channel53)).getText ());
+//        driver.findElement (By.cssSelector (TenetPage.channel53)).click ();
+//        driver.findElement (By.cssSelector (TenetPage.tvProgram)).click ();
+//        scrollToElement (driver.findElement(By.cssSelector (TenetPage.tvProgramDay)));
+//        System.out.println (driver.findElement(By.cssSelector (TenetPage.tvProgramDay)).getText ());
+
+
 
 
 
@@ -62,43 +61,50 @@ public class Tests {
 
     @Test
     public void tenet() throws InterruptedException {
-        WebElement element = driver.findElement (By.cssSelector (TenetPage.entrance));
-        element.click ();
-        WebElement element1 = driver.findElement (By.cssSelector (TenetPage.login));
-        element1.sendKeys (Variables.login);
-        Thread.sleep (1000);
-        WebElement element2 = driver.findElement (By.cssSelector (TenetPage.password));
-        element2.sendKeys (Variables.password);
-        Thread.sleep (1000);
+
+        String title = driver.getTitle ();
+        Assert.assertTrue (title.equals ("ТВ каналы онлайн — смотреть каналы ТВ онлайн в качестве прямой эфир онлайн"));
+        driver.findElement (By.cssSelector (TenetPage.entrance)).click ();
+        driver.findElement (By.cssSelector (TenetPage.login)).sendKeys (Variables.login);
+        driver.findElement (By.cssSelector (TenetPage.password)).sendKeys (Variables.password);
         driver.findElement (By.cssSelector (TenetPage.next)).click ();
-        Thread.sleep (2000);
         driver.findElement (By.cssSelector (TenetPage.categories)).click ();
         driver.findElement (By.cssSelector (TenetPage.channelCategories)).click ();
 
         int b = 0;
+        int c = 0;
+        List list = new ArrayList ();
         ChannelPage channelPage = new ChannelPage ();
+        for (String a : channelPage.addList ()){
         try {
-            for (String a : channelPage.addList ()){
-                WebElement element5 = driver.findElement (By.cssSelector (a));
-                scrollToElement (element5);
+                scrollToElement (driver.findElement (By.cssSelector (a)));
                 //Thread.sleep (1000);
-                element5.click ();
-                Thread.sleep (1000);
+                driver.findElement (By.cssSelector (a)).click ();
+               // Thread.sleep (1000);
                 b++;
-                System.out.println (" " +element5.getText ());
+                System.out.println (" " + driver.findElement (By.cssSelector (a)).getText ());
                 System.out.println ();
-            }
+            
         } catch (NoSuchElementException ignore) {
             System.out.println ("channel error");
+
+            list.add (a);
+            c++;
         } catch (ElementNotVisibleException ignore) {
 
-        }
+        }}
 
         System.out.println (b + " channel");
 
+        System.out.println (c);
+        for (Object e : list) {
+            System.out.println (e);
+
+        }
 
 
     }
+
 
     @Test
     public void incomingmail() throws InterruptedException {
