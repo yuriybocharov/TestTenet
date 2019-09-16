3db7b02fbf75d2d45bd4903fef669dc63a651b0a
yuriybocharov <bocharov0682@gmail.com>
2019-09-16 20:35:12 +0300

    test tv.tenet.ua

--- src/test/java/pages/TenetPage.java
+++ src/test/java/pages/TenetPage.java
@@ -13,7 +13,10 @@ public interface TenetPage {
     String categories = ".sb-down";
     String channelCategories = "#chcatlist_a";
     String tvprogram = "a.activetab:nth-child(1)";
-    String channel53 = "#chanlist_1370";
+    String channel53 = "#chanlist_1006";
+    String tvProgram = "a.activetab:nth-child(1)";
+    String tvProgramDay = "#tvprog";
+
     String channel77 = "#chanlist_242 > div:nth-child(2) > div:nth-child(1) > p:nth-child(1)";
 
 
