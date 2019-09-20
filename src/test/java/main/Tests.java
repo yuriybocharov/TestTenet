5cf4b6dcdc2ca810205cf677c2f80b036b40d8fc
yuriybocharov <bocharov0682@gmail.com>
2019-09-20 21:21:44 +0300

    test tv.tenet.ua

--- src/test/java/main/Tests.java
+++ src/test/java/main/Tests.java
@@ -32,62 +32,87 @@ public class Tests {
         driver.findElement (By.cssSelector (TenetPage.login)).sendKeys (Variables.login);
         driver.findElement (By.cssSelector (TenetPage.password)).sendKeys (Variables.password);
         driver.findElement (By.cssSelector (TenetPage.next)).click ();
-
-
-//        driver.findElement (By.cssSelector (TenetPage.channel53)).scrollToElement (element4);
-//        System.out.println (driver.findElement (By.cssSelector (TenetPage.channel53)).getText ());
-//        driver.findElement (By.cssSelector (TenetPage.channel53)).click ();
-//        driver.findElement (By.cssSelector (TenetPage.tvProgram)).click ();
-//        scrollToElement (driver.findElement(By.cssSelector (TenetPage.tvProgramDay)));
-//        System.out.println (driver.findElement(By.cssSelector (TenetPage.tvProgramDay)).getText ());
-
-    }
-
-
-    @Test
-    public void tenet() throws InterruptedException {
-
-        String title = driver.getTitle ();
-        Assert.assertTrue (title.equals ("ТВ каналы онлайн — смотреть каналы ТВ онлайн в качестве прямой эфир онлайн"));
-        driver.findElement (By.cssSelector (TenetPage.entrance)).click ();
-        driver.findElement (By.cssSelector (TenetPage.login)).sendKeys (Variables.login);
-        driver.findElement (By.cssSelector (TenetPage.password)).sendKeys (Variables.password);
-        driver.findElement (By.cssSelector (TenetPage.next)).click ();
-        driver.findElement (By.cssSelector (TenetPage.categories)).click ();
-        driver.findElement (By.cssSelector (TenetPage.channelCategories)).click ();
-
+        List<WebElement> elements = new ArrayList<> ();
+        List list = new ArrayList ();
+        WebElement element = driver.findElement (By.id ("inner_chanlist"));
+        elements = element.findElements (By.cssSelector ("a"));
         int b = 0;
         int c = 0;
-        List list = new ArrayList ();
-        ChannelPage channelPage = new ChannelPage ();
-        for (String a : channelPage.addList ()){
-        try {
-                scrollToElement (driver.findElement (By.cssSelector (a)));
-                //Thread.sleep (1000);
-                driver.findElement (By.cssSelector (a)).click ();
-               // Thread.sleep (1000);
+        for (WebElement a : elements) {
+
+            try {
+
+
+                scrollToElement (a);
+                a.click ();
                 b++;
-                System.out.println (" " + driver.findElement (By.cssSelector (a)).getText ());
+                System.out.println (" " + a.getText ());
                 System.out.println ();
-            
-        } catch (NoSuchElementException ignore) {
-            System.out.println ("channel error");
 
-            list.add (a);
-            c++;
-            b--;
-        } catch (ElementNotVisibleException ignore) {
+            }catch (NoSuchElementException ignore) {
+                System.out.println ("channel error");
 
-        }}
+                list.add (a);
+                c++;
+                b--;
+            } catch (ElementNotVisibleException ignore) {
+
+            }}
 
         System.out.println (b + " channel");
 
         System.out.println (c + "channel error");
         for (Object e : list) {
             System.out.println (e);
-
         }
+
     }
+
+
+//    @Test
+//    public void tenet() throws InterruptedException {
+//
+//        String title = driver.getTitle ();
+//        Assert.assertTrue (title.equals ("ТВ каналы онлайн — смотреть каналы ТВ онлайн в качестве прямой эфир онлайн"));
+//        driver.findElement (By.cssSelector (TenetPage.entrance)).click ();
+//        driver.findElement (By.cssSelector (TenetPage.login)).sendKeys (Variables.login);
+//        driver.findElement (By.cssSelector (TenetPage.password)).sendKeys (Variables.password);
+//        driver.findElement (By.cssSelector (TenetPage.next)).click ();
+//        driver.findElement (By.cssSelector (TenetPage.categories)).click ();
+//        driver.findElement (By.cssSelector (TenetPage.channelCategories)).click ();
+//
+//        int b = 0;
+//        int c = 0;
+//        List list = new ArrayList ();
+//        ChannelPage channelPage = new ChannelPage ();
+//        for (String a : channelPage.addList ()){
+//        try {
+//                scrollToElement (driver.findElement (By.cssSelector (a)));
+//                //Thread.sleep (1000);
+//                driver.findElement (By.cssSelector (a)).click ();
+//               // Thread.sleep (1000);
+//                b++;
+//                System.out.println (" " + driver.findElement (By.cssSelector (a)).getText ());
+//                System.out.println ();
+//
+//        } catch (NoSuchElementException ignore) {
+//            System.out.println ("channel error");
+//
+//            list.add (a);
+//            c++;
+//            b--;
+//        } catch (ElementNotVisibleException ignore) {
+//
+//        }}
+//
+//        System.out.println (b + " channel");
+//
+//        System.out.println (c + "channel error");
+//        for (Object e : list) {
+//            System.out.println (e);
+//
+//        }
+//    }
 
     @Test
     public void incomingmail() throws InterruptedException {
